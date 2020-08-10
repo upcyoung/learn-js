9db5a18dbd603c5d671d70a677f8412320d07dd0
upcyoung <bj_yangamo@hotmail.com>
2020-08-10 08:22:57 +0800

    Update have-a-rest.js

--- have-a-rest.js
+++ have-a-rest.js
@@ -43,6 +43,20 @@ for(var i=0;i<3;i++) {
   }
 }
 
-data[0]();
-data[1]();
-data[2]();
+
+
+function test () {
+   console.log('start')
+    setTimeout(() => {
+        console.log('children2')
+        Promise.resolve().then(() => {console.log('children2-1')})
+    }, 0)
+    setTimeout(() => {
+        console.log('children3')
+        Promise.resolve().then(() => {console.log('children3-1')})
+    }, 0)
+    Promise.resolve().then(() => {console.log('children1')})
+    console.log('end') 
+}
+
+test()
